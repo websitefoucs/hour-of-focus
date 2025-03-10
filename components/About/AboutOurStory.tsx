6247aa2b1a79b0a23019010c58afd69449c2d08d
Eran <michaelieran@gmail.com>
2025-03-10 18:02:56 +0200

    Fixed line height

--- components/About/AboutOurStory.tsx
+++ components/About/AboutOurStory.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 export default function AboutOurStory() {
   return (
-    <div className="grid grid-cols-2 mobile:grid-cols-1 mobile:grid-rows-[15rem,1fr] px-36 medium:px-16 mobile:px-4 gap-8 medium:gap-4 mobile:gap-8 ">
+    <div className="grid grid-cols-2 mobile:grid-cols-1 mobile:grid-rows-[15rem,1fr] px-36 medium:px-16 mobile:px-4 gap-8 medium:gap-4 mobile:gap-12 ">
       <article className="flex flex-col gap-8 mobile:order-1">
         <h5 className="">הסיפור שלנו</h5>
         <p className="text-18 text-mainGray-800 leading-10">{`פרויקט 'שעה של פוקוס' נוצר בפברואר 2024 על ידי מילואימניקים שהחליטו כי חשוב להמשיך בערבות ההדדית גם בעורף. מה שהתחיל כיוזמה צנועה עם מספר מתנדבים בודדים, צמח בהדרגה לפרויקט חינוכי משמעותי, הודות למאות מתנדבים שפועלים מתוך תחושת אחריות ועזרה לזולת.
