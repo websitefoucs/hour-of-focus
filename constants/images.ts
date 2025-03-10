c5f375deb00b4aef966ba1f93f5f0b41cad001c7
Eran <michaelieran@gmail.com>
2025-03-10 18:01:18 +0200

    .

--- constants/images.ts
+++ constants/images.ts
@@ -1,5 +1,4 @@
 export const LOGO = "/imgs/logo.svg";
-
 export const GINGER_KID_IMAGE =
   "https://res.cloudinary.com/dyzqa6uuu/image/upload/v1741441120/upuk4ge9uqdfqxhyax83.png";
 export const PEOPLE_ON_LAWN_IMAGE =
@@ -14,10 +13,8 @@ export const EXAM_IMAGE =
   "https://res.cloudinary.com/dyzqa6uuu/image/upload/v1741444552/wz1giguzo0z6cmofuiri.png";
 export const HANDS_IMAGE =
   "https://res.cloudinary.com/dyzqa6uuu/image/upload/v1741444552/pbshz6uien08wluyqc7p.png";
-
 export const WHO_WE_ARE_IMAGE =
   "https://res.cloudinary.com/dyzqa6uuu/image/upload/v1741440422/kuwcknnce1fnpxgkx5bs.png";
-
 export const TEAM_ONE_IMAGE =
   "https://res.cloudinary.com/dyzqa6uuu/image/upload/v1741443995/oydvartsojuxo8ki3lyu.png";
 export const TEAM_TWO_IMAGE =
