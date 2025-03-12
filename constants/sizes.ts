f7fdd1ca66934ebc79a91583b1cc9fada533a3ac
Eran <michaelieran@gmail.com>
2025-03-12 16:28:48 +0200

    clean-up

--- constants/sizes.ts
+++
@@ -1,8 +0,0 @@
-export const SIZES = {
-    extraSmall: "h-8 w-48",
-    small: "h-8 w-32",
-    medium: "h-12 w-[18.25rem]",
-    large: "h-12 w-60 ",
-    long: "h-[2.625rem] w-[20.875rem] leading-21",
-    none: "",
-  } as const;
--- types/toast.type.ts
+++
@@ -1,9 +0,0 @@
-export type TToast = {
-  id: string;
-  message: string;
-  type: TToastType;
-  createdAt: Date;
-};
-
-export const TOAST_TYPES = ["success", "error", "warning", "info"] as const;
-export type TToastType = (typeof TOAST_TYPES)[number];
