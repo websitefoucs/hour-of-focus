e2e75de859282e282503476024977dc4ed75dfaf
Eran <michaelieran@gmail.com>
2025-03-13 22:24:24 +0200

    Update article list width

--- components/Home/Articles/ArticlesList.tsx
+++ components/Home/Articles/ArticlesList.tsx
@@ -9,7 +9,7 @@ interface ArticleItemProps{
 
 export default function ArticlesList( {articles}:ArticleItemProps) {
   return (
-    <div className="h-fit lg:px-sides-sm px-sides flex flex-col items-center home-layout-articles">
+    <div className="h-fit w-full lg:px-sides-sm  flex flex-col items-center home-layout-articles">
       <h4 className="pb-8">כתבו עלינו</h4>
       <ItemsScroll<{
         publishPlace: string;
