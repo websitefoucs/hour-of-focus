b5dec7900ab389f81f28ff1d61a27e508951d431
Eran <michaelieran@gmail.com>
2025-03-13 12:14:58 +0200

    Add jsDocs to material cmps

--- components/Materials/MaterialItem.tsx
+++ components/Materials/MaterialItem.tsx
@@ -1,16 +1,31 @@
+//Types
 import { TMaterial } from "@/types/materials.type";
+//Next
 import Image from "next/image";
+//UI
 import LinkCmp from "../UI/LinkCmp";
 import { ArrowSvg } from "../UI/icons/Icons";
 
 interface MaterialItemProps {
   material: TMaterial;
 }
+/**
+ *Server Component that renders a students/teachers material item with an image, subject, and link to the google drive.
+ *
+ * @component
+ * @param {MaterialItemProps} props - The properties object.
+ * @param {Object} props.material - The material object.
+ * @param {string} props.material.imgPath - The path to the image of the material.
+ * @param {string} props.material.subject - The subject of the material.
+ * @param {string} props.material.link - The link to the material google drive.
+ * @returns {JSX.Element} The rendered MaterialItem component.
+ */
 export default function MaterialItem({ material }: MaterialItemProps) {
   const { imgPath, subject, link } = material;
 
   const text = `מאגר חומרי לימוד - ${subject}`;
   return (
+    //Use React fragment to handle the edit list in the admin page
     <>
       <Image
         src={imgPath || ""}
@@ -21,7 +36,12 @@ export default function MaterialItem({ material }: MaterialItemProps) {
       />
       <div className="p-4 flex flex-col h-[calc(45%-.5rem)] justify-between">
         <h5>{text}</h5>
-        <LinkCmp styleMode="arrow" styleSize="long" className="mobile:justify-between mobile:w-10 mobile-small:w-44" href={link}>
+        <LinkCmp
+          styleMode="arrow"
+          styleSize="long"
+          className="mobile:justify-between mobile:w-10 mobile-small:w-44"
+          href={link}
+        >
           <p>למאגר חומרי הלימוד</p>
           <ArrowSvg />
         </LinkCmp>
--- components/Materials/MaterialsIndex.tsx
+++ components/Materials/MaterialsIndex.tsx
@@ -1,12 +1,24 @@
+//Types
 import { TMaterial } from "@/types/materials.type";
+//React
 import React from "react";
+//UI
 import HeroCmp from "../UI/HeroCmp";
 import ItemList from "../UI/ItemList";
+//Components
 import MaterialItem from "./MaterialItem";
 
 interface MaterialsIndexProps {
   materials: TMaterial[];
 }
+/**
+ * MaterialsIndex server component renders a section containing a hero component and a list of materials for student and teachers.
+ *
+ * @param {MaterialsIndexProps} props - The props object containing the materials array.
+ * @param {Array} props.materials - An array of material objects to be displayed in the list.
+ *
+ * @returns {JSX.Element} The rendered MaterialsIndex component.
+ */
 export default function MaterialsIndex({ materials }: MaterialsIndexProps) {
   return (
     <section className="flex flex-col gap-16 pb-16 mobile:pb-12 w-full ">
