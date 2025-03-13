3a5912403d8e1ac13331b2c4f672b4f3ef1f0476
Eran <michaelieran@gmail.com>
2025-03-13 17:56:05 +0200

    Redo students info style

--- components/Explain/Students/ExplainStudentsInfo.tsx
+++ components/Explain/Students/ExplainStudentsInfo.tsx
@@ -3,27 +3,27 @@ import Image from "next/image";
 
 export default function ExplainVolunteersInfo() {
   return (
-    <div className="px-24 mobile:px-2 flex mobile:flex-col-reverse justify-around gap-36 mobile:gap-12 w-full py-24 mobile:py-12">
-      <article className="flex flex-col gap-6 mobile:gap-2 mobile:text-center">
+    <div className="sm:px-24 px-4 py-20 items-center grid grid-cols-1 xl:grid-cols-[60%_35%] w-full gap-10">
+      <article className="flex flex-col gap-3 font-normal ">
         <h5 className="">שיטת המעשים הטובים 🌍💙</h5>
 
-        <p className="text-mainGray-800 leading-30 text-18">
+        <h6 className="text-mainGray-500 leading-30 text-20 pb-3 font-normal">
           מעבירים את זה הלאה
-        </p>
-        <p className="text-mainGray-800 leading-30 text-18">{`בפרויקט "שעה של פוקוס", אנחנו מאמינים שלא רק הלמידה חשובה, אלא גם מעשים טובים שמייצרים שינוי חיובי. לכן, אנחנו מזמינים אתכם לבחור, אם תרצו, מעשה טוב עבור כל שיעור שתעברו בפרויקט.`}</p>
-        <p className="text-mainGray-800 leading-30 text-18">
+        </h6>
+        <p className="text-mainGray-800 leading-30 text-16 md:text-18">{`בפרויקט "שעה של פוקוס", אנחנו מאמינים שלא רק הלמידה חשובה, אלא גם מעשים טובים שמייצרים שינוי חיובי. לכן, אנחנו מזמינים אתכם לבחור, אם תרצו, מעשה טוב עבור כל שיעור שתעברו בפרויקט.`}</p>
+        <p className="text-mainGray-800 leading-30 text-16 md:text-18">
           <b>שימו לב:</b> מדובר בהמלצה בלבד – אין חובה להשתתף, אבל כל מעשה קטן
           יכול להשפיע בענק! 🌟
         </p>
       </article>
 
-      <div className="image-border before:bg-mainGold-500 after:bg-mainGold-500 relative  aspect-square min-w-[30rem] max-h-[25rem] mobile:min-w-0 mobile:h-64 ml-2 ">
+      <div className="image-border before:bg-mainGold-500 after:bg-mainGold-500 relative max-h-96 md:max-w-[36rem] xl:max-h-full xl:max-w-full w-full h-full place-self-center">
         <Image
           width={512}
           height={256}
           src={GIRL_PAINTING_IMAGE}
           alt="people on lawn"
-          className="w-full h-full rounded-tl-base rounded-br-base"
+          className="w-full h-full object-fill rounded-tl-base rounded-br-base"
         />
       </div>
     </div>
