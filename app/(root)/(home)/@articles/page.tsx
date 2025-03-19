3d2ba3c277e6cf9382ef559e0ee143381f568557
Eran <michaelieran@gmail.com>
2025-03-19 22:10:50 +0200

    Fix UI shift in articles

--- app/(root)/(home)/@articles/page.tsx
+++ app/(root)/(home)/@articles/page.tsx
@@ -1,7 +1,9 @@
 import ArticlesList from "@/components/Home/Articles/ArticlesList";
 import { getArticles } from "@/lib/actions/articles";
 import { cache } from "react";
+
 const cachedGetArticles = cache(getArticles);
+
 export default async function HomeArticlePage() {
   const articles = await cachedGetArticles(false);
   return <ArticlesList articles={articles} />;
--- components/Home/Articles/ArticleItem.tsx
+++ components/Home/Articles/ArticleItem.tsx
@@ -17,15 +17,14 @@ export default function ArticleItem({
 }: ArticleProps) {
   return (
     <div
-      className={`max-w-[49rem] h-72 w-full flex  gap-12 flex-col items-start border-t-8 border-t-mainGold-500 px-6 pt-12 pb-8 justify-between shadow-[0px_3px_6px_0px_#00000026] rounded-base transition-opacity duration-300
+      className={`max-w-[49rem] h-72 w-full flex  flex-col items-start border-t-8 border-t-mainGold-500 px-2 xs:px-6 pt-6 xs:pt-12 pb-8 justify-between shadow-[0px_3px_6px_0px_#00000026] rounded-base transition-opacity duration-300
        ${isFading ? "opacity-0" : "opacity-100"}`}
     >
-      <p className="text-right text-18 pr-1 text-mainGray-800 italic line-clamp-2">
+      <p className="text-right text-18 pr-1 text-mainGray-800 italic line-clamp-3">
         {preview}
       </p>
 
-      <p className="text-mainGray-500 text-18 text-right">
-        {" "}
+      <p className="text-mainGray-500 xs:text-18 text-16 text-right">
         <b>{publishPlace}</b>, {publishDate}
       </p>
       <LinkCmp
@@ -35,7 +34,7 @@ export default function ArticleItem({
         className="items-start place-items-start justify-start"
         target="_blank"
       >
-        <h6 className="text-right">לכתבה המלאה</h6>
+        <h6 className="text-right text-14">לכתבה המלאה</h6>
         <ArrowSvg />
       </LinkCmp>
     </div>
