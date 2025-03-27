a0649ee58ba3f445a7f769cf558f8f8beef87984
Eran <michaelieran@gmail.com>
2025-03-27 17:13:28 +0200

    Update not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,10 +1,13 @@
-import Image from 'next/image'
-import React from 'react'
+import LinkCmp from "@/components/UI/LinkCmp";
+import Image from "next/image";
+import React from "react";
 
 export default function NotFound() {
   return (
-    <div>
-        <Image src="/images/imgs.svg" alt="404" width={500} height={500} />
+    <div className="w-screen h-screen flex flex-col items-center justify-center justify-items-center">
+      <Image src="/imgs/notFound.svg" alt="404" width={500} height={500} />
+      <h1 className="text-mainGray-800">נראה לי הלכת לאיבוד</h1>
+      <LinkCmp styleMode="borderB" href="/">חזור לדף הבית</LinkCmp>
     </div>
-  )
+  );
 }
