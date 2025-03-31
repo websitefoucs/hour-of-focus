f578c347eea3bae37a485d5bb8f2dd8779b2d584
Eran <michaelieran@gmail.com>
2025-03-31 16:27:26 +0300

    Update image could url

--- constants/images.ts
+++ constants/images.ts
@@ -6,24 +6,25 @@ export const NOT_FOUND_IMAGE = "/images/imgs.svg";
 export const GINGER_KID_IMAGE =
   "https://res.cloudinary.com/dyzqa6uuu/image/upload/v1742072131/hof/piz0kiogdtnp8u9up22u.webp";
 export const PEOPLE_ON_LAWN_IMAGE =
-  "https://res.cloudinary.com/dyzqa6uuu/image/upload/v1741444113/hof/ydh48zfzq60e6payfzhw.png";
+  "https://res.cloudinary.com/dyzqa6uuu/image/upload/v1743425037/hof/smeulmtcotl97qdcnifr.webp";
 export const GIRL_PAINTING_IMAGE =
-  "https://res.cloudinary.com/dyzqa6uuu/image/upload/v1741449666/hof/lcc6sawf8kdprhwzbp4e.png";
+  "https://res.cloudinary.com/dyzqa6uuu/image/upload/v1743424775/hof/hd9grx28wczxbpriz2ye.webp";
 export const GIRL_STUDY_TWO_IMAGE =
-  "https://res.cloudinary.com/dyzqa6uuu/image/upload/v1741449373/hof/u4udhae82kmpd0ktikbn.png";
+  "https://res.cloudinary.com/dyzqa6uuu/image/upload/v1743424566/hof/amhjohyw6zdnv7qwieg1.webp";
 export const GIRL_STUDY_ONE_IMAGE =
-  "https://res.cloudinary.com/dyzqa6uuu/image/upload/v1741444552/hof/ak5k2obbvt3ozuvdhstk.png";
+  "https://res.cloudinary.com/dyzqa6uuu/image/upload/v1743424627/hof/mpcks6durhh3u8popaea.webp";
 export const EXAM_IMAGE =
-  "https://res.cloudinary.com/dyzqa6uuu/image/upload/v1741444552/hof/wz1giguzo0z6cmofuiri.png";
+  "https://res.cloudinary.com/dyzqa6uuu/image/upload/v1743424923/hof/dnmfngcciwwwdxjwwp6q.webp";
 export const HANDS_IMAGE =
-  "https://res.cloudinary.com/dyzqa6uuu/image/upload/v1741444552/hof/pbshz6uien08wluyqc7p.png";
+  "https://res.cloudinary.com/dyzqa6uuu/image/upload/v1743424835/hof/iopehe2tflo4b7tvqptw.webp";
 export const WHO_WE_ARE_IMAGE =
-  "https://res.cloudinary.com/dyzqa6uuu/image/upload/v1741440422/hof/kuwcknnce1fnpxgkx5bs.png";
+  "https://res.cloudinary.com/dyzqa6uuu/image/upload/v1743425105/hof/sqjc1vrkawtykwjwqug9.webp";
 export const TEAM_ONE_IMAGE =
-  "https://res.cloudinary.com/dyzqa6uuu/image/upload/v1741443995/hof/oydvartsojuxo8ki3lyu.png";
+  "https://res.cloudinary.com/dyzqa6uuu/image/upload/v1743427519/hof/ue8ilvhx1xpchqlcflot.webp";
 export const TEAM_TWO_IMAGE =
-  "https://res.cloudinary.com/dyzqa6uuu/image/upload/v1741443613/hof/aov3gfvfvektsfyyvjag.png";
-export const I_SCHOOL_IMAGE =
-  "https://res.cloudinary.com/dyzqa6uuu/image/upload/v1741629312/hof/osy6ipeqip77u4hymaqc.png";
+  "https://res.cloudinary.com/dyzqa6uuu/image/upload/v1743427344/hof/blq6dcqhsu0ea4gbhltc.webp";
 export const DEFAULT_IMAGE =
   "https://res.cloudinary.com/dyzqa6uuu/image/upload/v1742384690/hof/yeq1yyvb1tdfyuwuxfga.avif";
+  
+export const I_SCHOOL_IMAGE =
+  "https://res.cloudinary.com/dyzqa6uuu/image/upload/v1741629312/hof/osy6ipeqip77u4hymaqc.png";
