e34dab410b39a40b3a15a73d1408f80ef708b01c
P4t4m8n <michaelieran@gmail.com>
2025-08-12 15:37:23 +0300

    test

--- app/(root)/(home)/@testimonies/page.tsx
+++ app/(root)/(home)/@testimonies/page.tsx
@@ -5,5 +5,6 @@ import { getTestimonies } from "@/lib/actions/testimonies";
 
 export default async function HomeTestimoniesPage() {
   const testimonies = await getTestimonies(false);
+  console.log("🚀 ~ HomeTestimoniesPage ~ testimonies:", testimonies)
   return <Testimonies testimonies={testimonies} />;
 }
