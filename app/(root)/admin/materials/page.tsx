42b827b60002d730162471eaadaf5c75dc8ea411
Eran <michaelieran@gmail.com>
2025-10-28 14:38:39 +0200

    Remove AdminMaterialsPage component and associated materials logic

--- app/(root)/admin/materials/page.tsx
+++
@@ -1,7 +0,0 @@
-import AdminMaterialsIndex from "@/components/Admin/AdminMaterials/AdminMaterialsIndex";
-import { getMaterials } from "@/lib/actions/materials";
-
-export default async function AdminMaterialsPage() {
-  const materials = await getMaterials({ isFull: true });
-  return <AdminMaterialsIndex materials={materials} />;
-}
