f83222d08f3bf0198afe831579c6d2d8fa1adc98
Eran <michaelieran@gmail.com>
2025-11-30 17:15:08 +0200

    Add speed insight widget from Vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Open_Sans } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import Script from "next/script";
 
 const open_sans = Open_Sans({
@@ -84,6 +85,7 @@ export default function RootLayout({
       >
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
